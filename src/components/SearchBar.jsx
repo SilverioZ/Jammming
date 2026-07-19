4346e092af6c527d0f8326968f337662118dfa9f
Silverio L Zuniga Jr. <silvermoon12286@gmail.com>
2026-07-19 18:41:51 -0500

    SearchBar list

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function SearchBar({ allTracks, onSearch }) {
+    const [query, setQuery] = useState('');
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setQuery(value);
+
+        const filtered = allTracks.filter(track =>
+            track.name.toLowerCase().includes(value.toLowerCase()) ||
+            track.artist.toLowerCase().includes(value.toLowerCase()) ||
+            track.album.toLowerCase().includes(value.toLowerCase())
+        );
+
+        onSearch(filter);
+    };
+
+    return (
+        <input
+        type='text'
+        value={query}
+        onChange={handleChange}
+        placeholder='Search for songs, artists, or albums'
+        />
+    );
+}
+
+export default SearchBar;
